7d9050dfdc2af9999ab4a0f00d54c250b77d55d0
paritoshrawat <116328060+paritoshrawat@users.noreply.github.com>
2024-04-09 14:35:28 +0530

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,10 +5,10 @@ const Hero = () => {
   return (
     <section className={styles.container}>
       <div className={styles.content}>
-        <h1 className={styles.title}> hi, i am paritosh</h1>
+        <h1 className={styles.title}> Hi, i am paritosh</h1>
         <p className={styles.description}>
           {" "}
-          i am frontend developer with reactjs
+          I am MERN Stack Developer 
         </p>
         <a
           href="mailto: paritoshrawat21@gmail.com"
